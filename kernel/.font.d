59c8e85cd40cf02684e4510d0e6dc5fa0ed09fd8
nemunemu <rifa.kiyora@gmail.com>
2026-08-15 11:21:04 +0900

    day045

---
+++ kernel/.font.d
@@ -0,0 +1,5 @@
+font.o: font.cpp font.hpp \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  graphics.hpp frame_buffer_config.hpp
