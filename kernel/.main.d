88b0d52a6c6574a83b6874ac342603dc0cf17c7b
nemunemu <rifa.kiyora@gmail.com>
2026-08-15 11:21:04 +0900

    day045

---
+++ kernel/.main.d
@@ -0,0 +1,9 @@
+main.o: main.cpp \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
+  /home/ramur/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
+  frame_buffer_config.hpp font.hpp graphics.hpp console.hpp
