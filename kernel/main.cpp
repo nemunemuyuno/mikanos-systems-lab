930024254a3e6402fa40cdaa6353ad81ecf1dd05
nemunemu <rifa.kiyora@gmail.com>
2026-09-04 08:58:07 +0900

    day17

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -36,7 +36,7 @@
 #include "keyboard.hpp"
 #include "task.hpp"
 #include "terminal.hpp"
-
+#include "fat.hpp"
 
 
 
@@ -131,7 +131,8 @@ alignas(16) uint8_t kernel_main_stack[1024 * 1024];
 extern "C" void KernelMainNewStack(
     const FrameBufferConfig& frame_buffer_config_ref,
     const MemoryMap& memory_map_ref,
-    const acpi::RSDP& acpi_table) {
+    const acpi::RSDP& acpi_table,
+    void* volume_image) {
   MemoryMap memory_map{memory_map_ref};
 
   InitializeGraphics(frame_buffer_config_ref);
@@ -145,6 +146,7 @@ extern "C" void KernelMainNewStack(
   InitializeMemoryManager(memory_map);
   InitializeInterrupt();
 
+  fat::Initialize(volume_image);
   InitializePCI();
 
   InitializeLayer();
@@ -179,6 +181,24 @@ extern "C" void KernelMainNewStack(
   usb::xhci::Initialize();
   InitializeKeyboard();
   InitializeMouse();
+
+  // #@@range_begin(dump_volume)
+  uint8_t* p = reinterpret_cast<uint8_t*>(volume_image);
+  printk("Volume Image:\n");
+  for (int i = 0; i < 16; ++i) {
+    printk("%04x:", i * 16);
+    for (int j = 0; j < 8; ++j) {
+      printk(" %02x", *p);
+      ++p;
+    }
+    printk(" ");
+    for (int j = 0; j < 8; ++j) {
+      printk(" %02x", *p);
+      ++p;
+    }
+    printk("\n");
+  }
+  // #@@range_end(dump_volume)
 
   char str[128];
 
